334940c5eea2ddacb63c2de1b16d6c5792d1e75a
sdkag <senyo.k.agawu@gmail.com>
2020-08-09 14:41:34 -0400

    .

--- src/Components/CompanyList.js
+++ src/Components/CompanyList.js
@@ -2,8 +2,10 @@ import React, {setState} from 'react'
 import OpeningDetail from './OpeningDetail.js'
 
 export default function CompanyList({company}) {
+
 return <div>
-      <div>{company.company_name}</div>
-      <img src={company.image} alt="company-logo"/>
+      {JSON.stringify(company)}
+      {/* <div>{company.company_name}</div>
+      <img src={company.image} alt="company-logo"/> */}
     </div>
 }
