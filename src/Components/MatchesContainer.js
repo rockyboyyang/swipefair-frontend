deed6208614dcd59bec07067ce6e0e4efcf84508
sdkag <senyo.k.agawu@gmail.com>
2020-08-09 13:57:23 -0400

    add matches view

--- src/Components/MatchesContainer.js
+++ src/Components/MatchesContainer.js
@@ -1,31 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "../stylesheets/matches.css";
 import SwipeContainer from "./SwipeContainer";
+import CompanyList from './CompanyList'
 
-const MatchesContainer = (props) => { 
-  const jobseekerId = JSON.parse(localStorage.jobseeker).id
-  const herokuUrl = `https://boiling-sands-04799.herokuapp.com/api/jobseekers/${jobseekerId}/matches`;
-  const[matchesState, setMatchesState] = useState([])
-  const fetchMatches = async () => {
-      const res = await fetch(herokuUrl); // + '/'
-      const { matches } = await response.json();
-      setMatchesState(matches);
-  };
 
-    useEffect(() => {
-        fetchMatches();
-    }, [matchesState])
 
-    return (
+const MatchesContainer = ({matchesState}) => { 
+  // const jobseekerId = JSON.parse(localStorage.jobseeker).id
+    return ( matchesState.length ? 
         <div className="center-container">
+          div
+          <div>Matched with the Following Companies</div>
             {matchesState.map((match) => 
-            <MatchDetail match={match} setMatchesState={setMatchesState}/>)}
-        </div>
+              
+            <CompanyList match={match} /*setMatchesState={setMatchesState}*//>)}
+        </div> : <div>No matches yet</div>
     )
   
 
-  // return <div className="left-container">Hello from Matches Container</div>;
 };
 
 
--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-module.exports = {
-  awsAccessKeyID: process.env.AWS_ACCESS_KEY_ID,
-  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  apiBaseUrl : process.env.FLASK_API_BASE_URL || "http://localhost:3000/api"
-}
+export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : '/api';
+export const awsAccessKeyID = process.env.AWS_ACCESS_KEY_ID
+export const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
