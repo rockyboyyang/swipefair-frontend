658930720dbae99fb61cafb637e023a5393185d4
sdkag <senyo.k.agawu@gmail.com>
2020-08-09 14:30:52 -0400

    update branch

--- src/Components/CompanyList.js
+++ src/Components/CompanyList.js
@@ -1,8 +1,8 @@
 import React, {setState} from 'react'
 import OpeningDetail from './OpeningDetail.js'
 
-export default function CompanyList({opening, company}) {
-  return <div>
+export default function CompanyList({company}) {
+return <div>
       <div>{company.company_name}</div>
       <img src={company.image} alt="company-logo"/>
     </div>
