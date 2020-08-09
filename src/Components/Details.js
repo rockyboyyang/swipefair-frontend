1298d6b9b086611fce11d54af93f12b5203e0aeb
Rocky <yangrocky2013@gmail.com>
2020-08-08 22:04:07 -0500

    moved the info to messagesView so we can pass info to details component too

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -2,8 +2,8 @@ import React from "react";
 import "../stylesheets/details.css";
 // import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-const Details = () => {
-  return <div className="right-container">Hello from Details container</div>;
+const Details = ({ chattingWithInfoState, chattingWithName }) => {
+  return <div className="right-container">{chattingWithName}</div>;
 };
 
 export default Details;
