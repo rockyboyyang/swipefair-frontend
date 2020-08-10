0e2294cf4088665bba80b221997bdd164c923d53
Rocky <yangrocky2013@gmail.com>
2020-08-09 22:36:55 -0500

    added extra routes to grab more data.  Add some styling

--- src/Components/MatchesContainer.js
+++ src/Components/MatchesContainer.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "../stylesheets/matches.css";
 import CompanyList from "./CompanyList";
+import { useHistory, } from "react-router-dom";
 
 export default function MatchesContainer({ setMatchesState, matchesState }) {
   const jobseekerId = JSON.parse(window.localStorage.jobseeker).id;
   const jobseekerMatchesUrl = `http://localhost:5000/api/jobseekers/${jobseekerId}/matches`;
-
+  let history = useHistory();
   const fetchMatches = async () => {
     const res = await fetch(jobseekerMatchesUrl); // + '/'
     const response = await res.json();
@@ -24,19 +25,27 @@ export default function MatchesContainer({ setMatchesState, matchesState }) {
     const ans = {};
     arrOfObjs.forEach((value) => {
       if (ans[value.company.company_name] === undefined) {
-        ans[value.company.company_name] = {image: value.company.image, openings:[value.openings]};
+        ans[value.company.company_name] = {image: value.company.image, openings:[value.opening]};
       } else {
-        ans[value.company.company_name].openings.push(value.openings);
+        ans[value.company.company_name].openings.push(value.opening);
       }
     });
     return ans;
   };
 
+  const redirectToChats = () => {
+    history.push('/chats')
+  }
+
   if (matchesState.length) {
     const matches = combineCompanies(matchesState);
+    console.log(matches)
     return (
       <div className="left-container">
-        <div>Matched with the Following Companies</div>
+        <div className="match-header">
+          <h2>Matched with the Following Companies</h2>
+          <div onClick={redirectToChats}>CHATS</div>
+        </div>
         {Object.keys(matches).map((company_name) => (
           <CompanyList company_name={company_name} image={matches[company_name].image} openings={matches[company_name].openings} />
         ))}
