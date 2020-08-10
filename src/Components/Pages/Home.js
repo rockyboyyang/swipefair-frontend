eadd61a824f4665a676e86f0f048b264d9be0bfe
Rocky <yangrocky2013@gmail.com>
2020-08-10 10:00:11 -0500

    added redirection in case user does not have access.

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -4,6 +4,7 @@ import MatchesContainer from "../MatchesContainer";
 import SwipeContainer from "../SwipeContainer";
 import AboutUsContainer from "../AboutUsContainer";
 import '../../stylesheets/homeview.css'
+import { useHistory, } from "react-router-dom";
 // import {ourGet} from '../utils'
 
 
@@ -11,7 +12,10 @@ import '../../stylesheets/homeview.css'
 const Home = ({matchesState, setMatchesState, jobseekerState, companyState }) => {
     // const jobseekerId = JSON.parse(localStorage.jobseeker).id
     // useEffect(()=>{},[swipesState])
-
+  let history = useHistory()
+  if (!localStorage.access_token) {
+    history.push('/login')
+  }
   return (
     <div className='view-grid'>
       <Navbar />
