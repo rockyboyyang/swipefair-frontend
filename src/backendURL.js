1ef0bf12fc8be7fd276d243b7f37033d5921eb35
Rocky <yangrocky2013@gmail.com>
2020-10-05 12:09:57 -0500

    change backurl

--- src/backendURL.js
+++ src/backendURL.js
@@ -1,4 +1,4 @@
-const backendURL = 'http://localhost:5000/'
-// const backendURL = 'https://boiling-sands-04799.herokuapp.com/'
+// const backendURL = 'http://localhost:5000/'
+const backendURL = 'https://boiling-sands-04799.herokuapp.com/'
 
 export default backendURL;
